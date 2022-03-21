7f3830e09351edb2685c698d5af83f556348dd05
VineethaKadavakollu <100350369+VineethaKadavakollu@users.noreply.github.com>
2022-03-21 17:05:50 +0530

    Delete memoryused.js

--- memoryused.js
+++
@@ -1,8 +0,0 @@
-/**
- * @description calculates the memory used by the script
- * @returns memory used
- */
- module.exports = function memoryUsed() {
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    return Math.round(used * 100) / 100+" MB";
-}
