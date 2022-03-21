492e902d83dd69b6aea6d447fa92406042092280
VineethaKadavakollu <100350369+VineethaKadavakollu@users.noreply.github.com>
2022-03-21 17:03:52 +0530

    Delete CountLines.js

--- CountLines.js
+++
@@ -1,7 +0,0 @@
-const countLines = require('file-lines-count')
-/**
- * @description count the number of lines in the file
- */
- module.exports=function countLinesinFile(data) {
-   return (data.split("\n").length);
- }
