e6177a46ee536bd5932bb89502ca80d71c3b89db
VineethaKadavakollu <100350369+VineethaKadavakollu@users.noreply.github.com>
2022-03-21 17:05:24 +0530

    Delete filesInDirectory.js

--- filesInDirectory.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs')
-/**
- * @description counts no of files in the directory
- */
-module.exports = function filesinDirectory() {
-    const dir = 'C:/Users/kadavakollu.vineetha/Desktop/Tasks/Task1'
-    return fs.readdirSync(dir).filter(file => (fs.lstatSync(file).isFile())).length;
-}
